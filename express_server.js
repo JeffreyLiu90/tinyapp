3f9fa2c5314e90e471c4090123f97bc45639c527
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-17 23:17:05 +0000

    Tinyapp- created a page that allows you yo enter a long URL via POST method

--- express_server.js
+++ express_server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+  function generateRandomString() {
+    var result           = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+       for ( var i = 0; i < 7; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+ }
+
+
 
 app.set("view engine", "ejs");
 
@@ -31,7 +45,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); // goes to folder views, ejs file named urls_index and display the info
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]}; // the shortURL is the path name and what is after 
   res.render("urls_show", templateVars); // goes to folder views, ejs file named urls_index and display the info
 });// throws in the templateVars which define variables into the urls_show
+
+app.post("/urls", (req, res) => {
+console.log(req.body); // log the POST request body to console
+res.send("ok") // responds with OK (will be replaced)
+})
+
